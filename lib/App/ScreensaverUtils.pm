8c33e1d1eaf66b578a191c0c1e7a2fe7ec51b3e5
perlancar (on pc-office) <perlancar@gmail.com>
2017-03-30 18:30:14 +0700

    Initial commit

---
+++ lib/App/ScreensaverUtils.pm
@@ -0,0 +1,15 @@
+package App::ScreensaverUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to screensaver
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities related to screensaver:
+
+# INSERT_EXECS_LIST
+
+=back
