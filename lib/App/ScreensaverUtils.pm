7ad7d2b8e53d119385c6944fb12714b1a3dd23b8
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-30 11:30:39 +0700

    Add utility: prevent-screensaver-activated-while

--- lib/App/ScreensaverUtils.pm
+++ lib/App/ScreensaverUtils.pm
@@ -3,6 +3,43 @@ package App::ScreensaverUtils;
 # DATE
 # VERSION
 
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{prevent_screensaver_activated_while} = {
+    v => 1.1,
+    summary => 'Prevent screensaver activated while running a command',
+    description => <<'_',
+
+Uses <pm:Proc::Govern>. For more options when running command, e.g. timeout,
+load control, autorestart, use the module or its CLI <prog:govproc> instead.
+
+_
+    args => {
+        command => {
+            schema => ['array*', of=>'str*'],
+            req => 1,
+            pos => 0,
+            slurpy => 1,
+        },
+    },
+};
+sub prevent_screensaver_activated_while {
+    require Proc::Govern;
+
+    my %args = @_;
+
+    my $exit = Proc::Govern::govern_process(
+        command => $args{command},
+        no_screensaver => 1,
+    );
+
+    [200, "Exit code is $exit", "", {"cmdline.exit_code"=>$exit}];
+}
+
 1;
 # ABSTRACT: CLI utilities related to screensaver
 
